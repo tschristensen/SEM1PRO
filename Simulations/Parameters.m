57c5064fb4969e4916dd5075a237275d52789093
Martin Andersen <martinandersen25@gmail.com>
2016-05-04 22:02:23 +0200

    Started writing on Simulations

--- Simulations/Parameters.m
+++ Simulations/Parameters.m
@@ -4,17 +4,18 @@ m = 250;            % Total mass of the car with driver
 r = 11*.0254/2;     % Radius of driving wheels
 G1 = 12;            % Number of teeth on motor-side cog
 G2 = 50;            % Number of teeth on wheel-side cog
-J_G1 = (.01*G1/(2*pi)).^4*pi*8050*.01/2; %Inertia of motor-side cog
-J_G2 = (.01*G2/(2*pi)).^4*pi*8050*.01/2; %Inertia of wheel-side cog (assuming 1 cm per tooth, and 1 cm wide steel)
+J_G1 = 1.35554e-9*G1^4; %Inertia of motor-side cog
+J_G2 = 1.35554e-9*G2^4; %Inertia of wheel-side cog 
 c_drag = -1/2*1.3*.804*.6; %Drag coefficient. 
+c_drag_pro= c_drag*G1.^3*r.^3/G2.^3;
 
 % Motor parameters
 R=.013/2;             % Armature Resistance
-L=1e-4/2;             % Armature inductance
+L=4e-5;             % Armature inductance
 N=4;                % Number of pole pairs
 Kt=.07329;          % Kt (real version)
 J_M = 52e-4;        % Internal torque of the motor
-J_pro = m*r^2*G1/G2 + J_G2*G2/G2;% Projected inertia
+J_pro = (m*r^2+J_G2)*(G1/G2)^2 + J_G1;% Projected inertia
 
 
 Encoder_res = 512/N;  % Discrete angles that it can detect.
@@ -24,8 +25,6 @@ phase_a=cos(x+pi/2);
 phase_b=cos(x+pi/2-2*pi/3);
 phase_c=cos(x+pi/2+2*pi/3);
 phase_3=-1/6*cos(3*(x+pi/2));
-
-
 
 % Simulation parameters
 ts=1/20000;             % Sampling time for discrete subsystems
